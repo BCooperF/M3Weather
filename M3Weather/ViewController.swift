7f17cd30420e4be1d159c5146831549e6d258e7f
BCooperF <bradley_faesy@mymail.eku.edu>
2026-03-17 11:51:45 -0400

    M7 Weather

--- M3Weather/ViewController.swift
+++ M3Weather/ViewController.swift
@@ -8,60 +8,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-	
-	
-	@IBOutlet weak var weatherImage: UIImageView!
+
+	@IBOutlet weak var weatherImageView: UIImageView!
 	@IBOutlet weak var statusLabel: UILabel!
-	@IBOutlet weak var tempLabel: UILabel!
-	@IBOutlet weak var descriptLabel: UILabel!
+	@IBOutlet weak var temperatureLabel: UILabel!
 	
+	@IBOutlet weak var descriptionLabel: UILabel!
 	
+	func getWeatherImage (matching weather: String) -> UIImage? {
+		switch weather{
+		case "Sunny":
+			return UIImage(named:"sunny")
+		case "Cloudy":
+			return UIImage(named:"cloudy")
+		case "Rainy":
+			return UIImage(named:"rainy")
+		case "Snowy":
+			return UIImage(named:"snowy")
+		default:
+			return UIImage(named:"weather")		}
+	}
+	
+	func getTemp (matching temp: Int) -> String{
+		switch temp{
+		case 0..<20:
+			return "Extream cold"
+		case 20..<50:
+			return "It's cold outside"
+		case 50..<69:
+			return "Cool and mild"
+		case 69..<86:
+			return "Warm!"
+		default:
+			return "Heat alert!"
+		}
+	}
+	
+	/*
+	 func currencySymbol(for: country: String?) -> String?{
+		switch country{
+		case "US":
+			return "$"
+		case "DE":
+			return "E"
+		case "KR":
+			return "W"
+		case "JP":
+			return "Y"
+		default:
+			nil
+		}
+	}
+	 
+	 if let cs = currencySymbol(for: "US"){
+		print(cs)
+	 }else{
+		print("Not Found")
+	 }
+	 
+	--------------
+	let input: String? = "42.5"
+	 
+	 let fu = Double(input!)!
+	 print(fu) //Forced Unwrap
+	 
+	 if let input, let ob = Double(input){
+		print(ob) //Optional Binding
+	 }else{
+		print("Invalid")
+	 }
+	 
+	 let nc = Double(input ?? "0") ?? 0.0
+	 print(nc) //Nil-Coalescing
+	 --------------
+	 let nickname: String? = "SwiftCoder"
+	 
+	 let fu = Double(input!)!
+	 print(fu) //Forced Unwrap
+	 
+	 if let input, let ob = Double(input){
+		print(ob) //Optional Binding
+	 }else{
+		print("Invalid")
+	 }
+	 
+	 let nc = Double(input ?? "0") ?? 0.0
+	 print(nc) //Nil-Coalescing
+	*/
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		let weather = ["Sunny", "Cloudy", "Rainy", "Snowy"].randomElement() ?? "Sunny"
+		let temperature = Int.random(in: 0 ... 100)
 		
-		let weather = "Snowy"
-		let temp = 78
-		
-		/*if weather == "Sunny" {
-			weatherImage.image = UIImage(named: "sun")
-		}else if weather == "Rainy" {
-			weatherImage.image = UIImage(named: "heavy-rain")
-		}else if weather == "Snowy" {
-			weatherImage.image = UIImage(named: "snowy")
-		}else if weather == "Cloudy" {
-			weatherImage.image = UIImage(named: "cloudy")
-		}else{
-			weatherImage.image = UIImage(named: "meteorology")
-		}*/
-		
-		switch weather {
-		case "Sunny":
-			weatherImage.image = UIImage(named: "sunny")
-		case "Rainy":
-			weatherImage.image = UIImage(named: "heavy-rain")
-		case "Snowy":
-			weatherImage.image = UIImage(named: "snowy")
-		case "Cloudy":
-			weatherImage.image = UIImage(named: "cloudy")
-		default:
-			weatherImage.image = UIImage(named: "meteorology")
-		}
-		
+		weatherImageView.image = getWeatherImage(matching: weather)
 		statusLabel.text = weather
-		tempLabel.text = "\(temp)"
-		
-		if temp <= 19 {
-			descriptLabel.text = "Freezing"
-		}else if temp >= 20 && temp <= 49 {
-			descriptLabel.text = "Chilly"
-		}else if temp >= 50 && temp <= 70 {
-			descriptLabel.text = "Warm"
-		}else if temp >= 71 && temp <= 90 {
-			descriptLabel.text = "Hot"
-		}else{
-			descriptLabel.text = "EXTREME"
-		}
+		temperatureLabel.text = "\(temperature)"
+		descriptionLabel.text = getTemp(matching: temperature)
 	}
 }
-
